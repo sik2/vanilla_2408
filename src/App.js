2b2edbd9a4e62766a82fce7c1e6bd14edcbae426
sik2 <bickwang123@gmail.com>
2024-08-05 21:30:40 +0900

    add 로컬 스토리지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from "./components/Header.js";
 import TodoForm from "./components/TodoForm.js";
 import TodoList from "./components/TodoList.js";
+import { getItem, setItem } from "./storage.js";
 
 export default function App({ $target }) {
-  const intialState = [
+  const intialState = getItem("todos", [
     { id: 1, text: "item1", checked: true },
     { id: 2, text: "item2", checked: false },
     { id: 3, text: "item3", checked: false },
-  ];
+  ]);
 
   const length = intialState.length;
   let lastId = intialState[length - 1].id;
@@ -24,11 +25,13 @@ export default function App({ $target }) {
     console.log(nextState);
     lastId++;
     todoList.setState(nextState);
+    setItem("todos", JSON.stringify(nextState));
   };
 
   const onDelete = (id) => {
     const nextState = todoList.state.filter((todo) => todo.id !== id);
     todoList.setState(nextState);
+    setItem("todos", JSON.stringify(nextState));
   };
 
   const onToggle = (id) => {
@@ -37,6 +40,7 @@ export default function App({ $target }) {
     );
     console.log(nextState);
     todoList.setState(nextState);
+    setItem("todos", JSON.stringify(nextState));
   };
 
   const header = new Header({ $target });
